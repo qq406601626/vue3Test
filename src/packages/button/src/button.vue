46a1049b5274832fe922ec6512e4b91356f4f03b
guoshuai <406601626@qq.com>
2022-02-18 18:07:45 +0800

    修改

--- src/packages/button/src/button.vue
+++ src/packages/button/src/button.vue
@@ -2,13 +2,14 @@
   <div>button</div>
 </template>
 
-<script lang="ts">
-import {defineComponent} from 'vue';
-
-export default defineComponent({
-  components: {},
-  setup() {
-    return {}
-  },
-});
+<script lang="ts" setup>
+// defineProps({
+//   size: {
+//     type: String,
+//     validator(value: string) {
+//       // 这个值必须与下列字符串中的其中一个相匹配
+//       return ['default', 'small', 'large'].includes(value)
+//     }
+//   }
+// })
 </script>
---
+++ src/views/button.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-button></el-button>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import ElButton from '@/packages/button/src/button.vue'
+
+export default defineComponent({
+  components: {ElButton},
+});
+</script>
