58aa756f7656c463c38aa9903c69b19131772256
guoshuai <406601626@qq.com>
2022-02-18 14:02:38 +0800

    修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <div class="home"></div>
 </template>
 
 <script lang="ts">
-import { defineComponent,ref } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {defineComponent} from 'vue';
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  components: {},
+  setup() {
+    return {}
   },
-  setup(){
-    const count = ref(0)
-    console.log(count) // 0
-    return {
-      name:ref('111')
-    }
-  },
-  mounted() {
-    console.log(this.name)
-  }
 });
 </script>
