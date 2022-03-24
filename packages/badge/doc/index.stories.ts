155c45ddc03cb4978133cb6aff8a5cb887ab7749
guoshuai <406601626@qq.com>
2022-03-24 10:58:09 +0800

    修改

---
+++ packages/badge/doc/index.stories.ts
@@ -0,0 +1,10 @@
+import Basic from './index.vue'
+import ElBadge from '../index'
+import ElButton from '@element-plus/button'
+
+export default {
+    title: "Badge",
+    component: [ElButton, ElBadge]
+}
+
+export const BasicUsage = () => Basic
--- packages/button/doc/index.stories.ts
+++ packages/button/doc/index.stories.ts
@@ -2,11 +2,9 @@ import ElButton from '../index';
 
 export default {
     title: 'Button',
+    component: ElButton,
 };
 
-export const NormalButton = () => {
-    return { template: '<el-button>With Text</el-button>', installer: ElButton };
-};
-export const ButtonTwo = () => {
-    return { template: '<el-button>button two</el-button>', installer: ElButton };
-};
+export const NormalButton = () => '<el-button>With Text</el-button>'
+export const ButtonTwo = () => '<el-button>button two</el-button>'
+export const ButtonThree = () => '<el-button type="primary" :loading="true">button three</el-button>'
